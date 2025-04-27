0e1455912515fc44d0c69b0b59ee84c1f686166d
Nishant Saini <ns0109375@gmail.com>
2025-04-27 19:19:21 +0530

    modify eslint file

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,7 @@
 import { dirname } from "path";
 import { fileURLToPath } from "url";
 import { FlatCompat } from "@eslint/eslintrc";
+import { config as tseslintConfig } from "typescript-eslint";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -11,14 +12,14 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   {
-    ignores: ["src/generated/**"], 
+    ignores: ["src/generated/**"],
   },
-  {
+  ...tseslintConfig({
     rules: {
       "no-unused-vars": "off",
       "@typescript-eslint/no-unused-vars": "error",
     },
-  },
+  }),
   ...compat.extends("next/core-web-vitals", "next/typescript"),
 ];
 
